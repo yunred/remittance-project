a9fd6d26c70660b9d51d9d775b0d0416914f8912
SEOYUN KIM <nado0903@gmail.com>
2021-07-23 15:18:14 +0900

    feat: Deposit component 화면 구성, add 체크박스 onClick이벤트

--- src/Deposit.js
+++ src/Deposit.js
@@ -4,7 +4,21 @@ import styled from 'styled-components';
 import axios from 'axios';
 import Template from './components/Template';
 import Back from './components/Back';
+import Button from './components/ButtonStyle';
+import check from './check.png';
 
+
+const DepositBlock = styled.div`
+  height: 55vh;
+  display: flex;
+  flex-direction: column; 
+  justify-content: space-between;
+
+  .num{
+    font-size: 16px;
+    color: gray;
+  }
+`;
 
 const RecipientBlock = styled.div`
   display: flex;
@@ -22,12 +36,8 @@ const RecipientBlock = styled.div`
     font-weight : bold;
   }
 
-  .num{
-    font-size: 16px;
-    color: gray;
-  }
-`;
 
+`;
 
 const RecipientCircle = styled.div`
   width: 60px;
@@ -35,6 +45,7 @@ const RecipientCircle = styled.div`
   border-radius: 30px;
   border: 1px solid #ced4da;
   display: flex;
+  align: left;
   align-items: center;
   justify-content: center;
   margin: auto 8px;
@@ -47,8 +58,77 @@ const RecipientCircle = styled.div`
 
 `;
 
+const SenderBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 12px; 
+`;
+
+const PersonBlock = styled.button`
+  height: 70px;
+  display: flex;
+  flex-direction: row; 
+  background: none;
+  border: none;
+  &:active {
+    background: #F5FFFA;
+  }
+`;
+const Circle = styled.div`
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+  border: 1px solid #ced4da;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: auto 8px;
+
+  img {
+    width: 32px;
+    height: 32px;
+    object-fit: cover;
+  }
+
+`;
+
+const Check = styled.div`
+  width: 48px;
+  height: 48px;
+  border: 1px none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: auto 8px;
+  margin-left: auto; 
+
+  img {
+    width: 32px;
+    height: 32px;
+    object-fit: cover;
+    visibility: ${props=>props.index === props.checked ? 'visible': 'hidden'}
+  }
+
+`;
+const AccountImfo= styled.div`
+  height: 50px;
+  background: none;
+  text-align: left;
+  padding: 8px 16px;
+  line-height : 5%;
+  .c1{
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .c2{
+    font-size: 14px;
+    color: #a9a9a9;
+  }
+`;
 
 function Deposit( {history} ){
+  const [checked, setChecked] = useState(0);
   const [accounts, setAccounts] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -83,12 +163,13 @@ function Deposit( {history} ){
   if (loading) return <div>로딩중</div>;
   if (error) return <div>에러 발생</div>;
   if (!accounts) return null;
-  // <br/><span>{person.bankName}{person.accountNumber}</span>
 
   return(
     <>
     <Template>
     <Back history = {history} value = {value}/>
+    <DepositBlock>
+      
     <RecipientBlock>
       <p><span>{person.accountHolder}</span>님 계좌로 <br/>
       <span>{value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span> 을 보냅니다</p>
@@ -96,8 +177,23 @@ function Deposit( {history} ){
       <p className ="num">{person.bankName} {person.accountNumber}</p>
     </RecipientBlock>
 
+    <SenderBlock>
+      <p className="num">출금 계좌</p>
+      {accounts.map((account, index) => {
+        return <PersonBlock key = {account._id} onClick= {()=>setChecked(index)}>
+          <Circle><img src={account.bankImageUrl}/></Circle>
+          <AccountImfo>
+            <p className="c1">{account.accountName}</p>
+            <p className="c2">{account.accountBalance}</p>
+          </AccountImfo>
+          <Check index={index} checked={checked}><img src={check} alt='check'/></Check>
+        </PersonBlock>
+      })}
+    </SenderBlock>
+    </DepositBlock>
 
-    
+
+    <Button onClick= {()=>{}} Send = {true}>보내기</Button>
     </Template>
     </>
   )
