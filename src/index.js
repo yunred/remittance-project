ca9b22a5dbe5c4b426ec515e2706d95499a5d060
SEOYUN KIM <nado0903@gmail.com>
2022-03-02 02:03:12 +0900

    Fix: 오류 수정

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
-import rootReducer from './redux';
+import rootReducer from 'redux/index';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
