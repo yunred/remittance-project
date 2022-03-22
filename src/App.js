b95b4b4eb63041ec3796c3e2e1e88090ea96fe6c
SEOYUN KIM <nado0903@gmail.com>
2022-03-22 14:02:09 +0900

    Modify: 중복 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Money from 'pages/Money/index.Money.js';
 import List from 'pages/List/index.List.js';
 import GlobalStyle from 'styles/Globalstyle.js';
@@ -10,16 +10,14 @@ import Template from 'common/Template';
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Template>
-          <Routes>
-            <Route path="/" element={<Money />} />
-            <Route path="/list" element={<List />} />
-            <Route path="/deposit" element={<Deposit />} />
-          </Routes>
-        </Template>
-      </BrowserRouter>
+      <GlobalStyle />
+      <Template>
+        <Routes>
+          <Route path="/" element={<Money />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/deposit" element={<Deposit />} />
+        </Routes>
+      </Template>
     </>
   );
 }
